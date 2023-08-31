f23bb9e1dd47bd59bbae3e762718afadad66c320
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-31 13:56:01 +0700

    feat: add exchange_rate.py model

---
+++ model/exchange_rate.py
@@ -0,0 +1,11 @@
+from _decimal import Decimal
+from pydantic import BaseModel, Field
+
+from model.currency import Currency
+
+
+class ExchangeRate(BaseModel):
+    id: int
+    base_currency: Field(Currency)
+    target_currency: Field(Currency)
+    rate: Decimal
