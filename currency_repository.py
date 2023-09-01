88ae81b80d0362e9e82b50a7f72c49fb64ad6739
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-01 14:10:44 +0700

    feat(repository): add `find_all` method to the currency_repository.py

--- currency_repository.py
+++ currency_repository.py
@@ -4,11 +4,15 @@ import schema.currency
 from models.currency import Currency
 
 
-def find_currency_by_code(db: Session, code: str):
+def find_all(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Currency).offset(skip).limit(limit).all()
+
+
+def find_by_code(db: Session, code: str):
     return db.query(Currency).filter(Currency.code == code).first()
 
 
-def save_currency(db: Session, currency_schema: schema.currency.Currency):
+def save(db: Session, currency_schema: schema.currency.CurrencyAdd):
     currency = Currency(**currency_schema.model_dump())
 
     db.add(currency)
