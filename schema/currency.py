ac565c90451e46930e637a3dd3d86edeb444d076
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-01 14:12:08 +0700

    refactor(schema): add CurrencyBase & CurrencyAdd models

--- schema/currency.py
+++ schema/currency.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel, constr
 
 
-class Currency(BaseModel):
-    id: int
+class CurrencyBase(BaseModel):
     code: constr(min_length=3, max_length=3)
     full_name: str
     sign: str
 
+
+class Currency(CurrencyBase):
+    id: int
+
     class Config:
         from_attributes = True
+
+
+class CurrencyAdd(CurrencyBase):
+    pass
