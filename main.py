5d23cee5aec78b222169029c0ef239bbf88f1972
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-01 14:12:41 +0700

    feat: add currencies operation to the main.py

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from starlette.requests import Request
+
+import currency_repository
+from database import SessionLocal, Base, engine
+from schema.currency import Currency, CurrencyAdd
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return {"message": f"For the swagger docs visit {request.url}docs"}
+
+
+@app.get("/api/currencies", response_model=list[Currency])
+async def get_currencies(db: Session = Depends(get_db)):
+    return currency_repository.find_all(db)
+
+
+@app.post("/api/currencies", response_model=Currency)
+async def post_currency(currency: CurrencyAdd, db: Session = Depends(get_db)):
+    return currency_repository.save(db, currency)
+
+
+@app.get("/api/currency/{code}", response_model=Currency)
+async def get_currency(code, db: Session = Depends(get_db)):
+    return currency_repository.find_by_code(db, code)
