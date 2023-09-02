246c8874f89b65d521f39eb6739cf72784b93712
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-02 14:31:16 +0700

    refactor(schema): divide one ExchangeRate schema to 3

--- schema/exchange_rate.py
+++ schema/exchange_rate.py
@@ -1,14 +1,24 @@
 from _decimal import Decimal
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, constr
 
 from schema.currency import Currency
 
 
-class ExchangeRate(BaseModel):
-    id: int
+class ExchangeRateBase(BaseModel):
     base_currency: Field(Currency)
     target_currency: Field(Currency)
     rate: Decimal
 
     class Config:
         from_attributes = True
+        arbitrary_types_allowed = True
+
+
+class ExchangeRate(ExchangeRateBase):
+    id: int
+
+
+class ExchangeRateAdd(BaseModel):
+    base_currency_code: constr(min_length=3, max_length=3)
+    target_currency_code: constr(min_length=3, max_length=3)
+    rate: Decimal
