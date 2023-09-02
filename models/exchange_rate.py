e112c723552cf5aa0828d799253b935195a4c592
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-02 14:30:33 +0700

    refactor(model): add foreign keys to exchange_rate.py model

--- models/exchange_rate.py
+++ models/exchange_rate.py
@@ -1,4 +1,5 @@
-from sqlalchemy import DECIMAL, UniqueConstraint
+from _decimal import Decimal
+from sqlalchemy import UniqueConstraint, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
@@ -7,9 +8,14 @@ from models.currency import Currency
 
 class ExchangeRate(Base):
     __tablename__ = "exchange_rates"
-    __table_args__ = UniqueConstraint("base_currency", "target_currency")
+    __table_args__ = (
+        UniqueConstraint("base_currency_id", "target_currency_id"),
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    base_currency: Mapped["Currency"] = relationship(back_populates="id")
-    target_currency: Mapped["Currency"] = relationship(back_populates="id")
-    rate: Mapped[DECIMAL] = mapped_column()
+    base_currency_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"))
+    target_currency_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"))
+    rate: Mapped[Decimal] = mapped_column()
+
+    base_currency: Mapped["Currency"] = relationship("Currency", foreign_keys=[base_currency_id])
+    target_currency: Mapped["Currency"] = relationship("Currency", foreign_keys=[target_currency_id])
